36347034f2461bc529df5ce78912d5e7c16eddd4
COMVIVA\srivatsan.n <srivatsan.n@comviva.com>
2023-11-15 21:04:38 +0530

    commit for model validation and dependency+++

--- src/main/java/com/api/employee/model/Employee.java
+++ src/main/java/com/api/employee/model/Employee.java
@@ -1,13 +1,17 @@
 package com.api.employee.model;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Employee {
-    private  int empId;
+    private int empId;
+    @Size(min = 3, message = "Name should have minimum 3 characters")
     private String empName;
+    @Size(min = 3, message = "Department should have minimum 3 characters")
     private String empDept;
+    @Size(min = 3, message = "Role should have minimum 3 characters")
     private String empRole;
 }
