3c0012127ffb9553845f342a41d19a48a23bbebe
COMVIVA\srivatsan.n <srivatsan.n@comviva.com>
2023-11-16 09:58:36 +0530

    custom exception implemented feature/custom-exception

---
+++ src/main/java/com/api/employee/model/ErrorDetails.java
@@ -0,0 +1,13 @@
+package com.api.employee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ErrorDetails {
+    private String message;
+    private String description;
+}
